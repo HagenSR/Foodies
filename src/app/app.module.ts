f224cf9100536f2b2b50cd057f31c3b943b227aa
terrainbuilder52 <32080764+terrainbuilder52@users.noreply.github.com>
2020-04-07 18:14:26 -0500

    Added routing for linking places

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+
+const appRoutes: Routes = [
+  { path: 'landingpage', component: LandingpageComponent },
+
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +18,11 @@ import { LandingpageComponent } from './landingpage/landingpage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
