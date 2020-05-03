bf22e2368496b0d4f2380df5da4aa806f08b83d5
Sophie <sophie.noel@ndsu.edu>
2020-05-02 21:39:25 -0500

    Database stuff and editing works... finally
    Now to allow user to edit their profile information upon sign up and do cosmetics!

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthenticationService } from '../authentication.service';
 import { Router } from '@angular/router';
+import { FoodService } from 'src/app/food.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,7 +13,7 @@ export class SignUpComponent implements OnInit {
   registerForm: FormGroup;
   authError : any;
   submitted = false;
-  constructor(private formBuilder: FormBuilder, private auth:AuthenticationService, private route : Router) { }
+  constructor(private formBuilder: FormBuilder, private auth:AuthenticationService, private route : Router, private foodService: FoodService) { }
 
   ngOnInit(): void {
     this.auth.eventAuthError$.subscribe(data =>{
@@ -32,6 +33,10 @@ get f() { return this.registerForm.controls; }
 
 
 createUser(){
+  
+
   this.auth.createUser(this.registerForm.value);
+
+  this.foodService.addUserProfileInformation(this.registerForm.value["email"]);
 }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,38 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FoodService } from '../food.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [AngularFireAuth, FoodService],
 })
 export class ProfileComponent implements OnInit {
 
-  sex = "female"
+  @Input() informationArray: any[] = [];
+  //informationArray=[10,12];
 
-  weight =145;//default values till profile created from sign in.
-  height = 66
-  bmi = Math.round( (703 * this.weight / Math.pow(this.height,2)) * 10 ) / 10 //Shenanigans because round functions rounds to integer
-  bmiResults = ""
+  sex: string = "male";
+  weight: number;
+  height: number;
+  exercise: number;
+  smoke: number;
+  drink: number;
+  bmi: number;
+  bmiResults: string = "AHHHHH";
+  
+  
+  profileForm = this.fb.group({
+    weightForm: [this.weight],
+    heightForm: [this.height],
+    smokeForm: [this.smoke],
+    exerciseForm: [this.exercise],
+    drinkForm: [this.drink],
+  });
+  
 
-  smoke = 0
-  exercise = 3
-  drink = 15
-
-  constructor() { }
+  constructor(private fb: FormBuilder, private foodService: FoodService, private afAuth : AngularFireAuth) { }
 
   ngOnInit(): void {
-    if (this.bmi < 18.5) {
-      this.bmiResults = "underweight"
-    }
-    else if(this.bmi < 25) {
-      this.bmiResults = "normal weight"
-    }
-    else if(this.bmi < 30){
-      this.bmiResults = "overweight"
-    }
-    else{
-      this.bmiResults = "obese"
-    }
+    //this.informationArray=[10,12];
+    let userEmail : string;
+    this.afAuth.authState.subscribe(data =>{
+      this.foodService.getUserProfileInformation(data.email).then(data =>{
+        this.informationArray = data;
+        this.update();
+      })
+    })
+
+    
+
+    //this.informationArray = this.afAuth.currentUser.then(tmps => this.foodService.getUserProfileInformation(tmps.email));
+    //this.informationArray = this.foodService.getUserProfileInformation()
+    
+  }
+
+  ngOnChanges(): void {
+    console.log("Something changed!")
+  }
+
+  update() {
+
+
+    
+
+        //This is in the Async code because... async shenanigans
+        //Note: informationArray = [sex, weight, height, smoke, exercise, drink]
+        this.sex = this.informationArray[0];
+        this.weight = this.informationArray[1];
+        this.height = this.informationArray[2];
+        this.bmi = Math.round( (703 * this.weight / Math.pow(this.height,2)) * 10 ) / 10 //Shenanigans because round functions rounds to integer
+
+        this.smoke = this.informationArray[3];
+        this.exercise = this.informationArray[4];
+        this.drink = this.informationArray[5];
+
+        if (this.bmi < 18.5) {
+          this.bmiResults = "underweight"
+        }
+        else if(this.bmi < 25) {
+          this.bmiResults = "normal weight"
+        }
+        else if(this.bmi < 30){
+          this.bmiResults = "overweight"
+        }
+        else if(this.bmi >= 30){
+          this.bmiResults = "obese"
+        }
+
+        this.profileForm = this.fb.group({
+          weightForm: [this.weight],
+          heightForm: [this.height],
+          smokeForm: [this.smoke],
+          exerciseForm: [this.exercise],
+          drinkForm: [this.drink],
+        });
+  }
+  
+
+  onSubmit() {
+
+    this.informationArray[1] = this.profileForm.value["weightForm"];
+    this.informationArray[2] = this.profileForm.value["heightForm"];
+    this.informationArray[3] = this.profileForm.value["smokeForm"];
+    this.informationArray[4] = this.profileForm.value["exerciseForm"];
+    this.informationArray[5] = this.profileForm.value["drinkForm"];
+
+    this.afAuth.authState.subscribe(data =>{
+      of((this.foodService.editUserProfileInformation(data.email, this.informationArray))).subscribe(garbage => this.update())
+    })
+    
+
   }
 
 }
