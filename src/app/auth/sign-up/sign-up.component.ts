029bc0900fe9355390803cbb6129abdead29164f
Sophie <sophie.noel@ndsu.edu>
2020-05-07 14:39:17 -0500

    Got rid of old comments

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -11,9 +11,7 @@ import { FoodService } from 'src/app/food.service';
 })
 export class SignUpComponent implements OnInit {
   registerForm: FormGroup;
-  //profileInformationForm: FormGroup;
   authError : any;
-  //profileInformation: any[] = [];
   submitted = false;
   constructor(private formBuilder: FormBuilder, private auth:AuthenticationService, private route : Router, private foodService: FoodService) { }
 
@@ -27,9 +25,6 @@ export class SignUpComponent implements OnInit {
       lastName: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      //weight: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
-      // height: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
-      // sex: ['', Validators.required],
   });
 
 
@@ -44,37 +39,7 @@ export class SignUpComponent implements OnInit {
       this.foodService.addUserProfileInformation(x.toString());
       console.log(x.toString() + " vs " + this.registerForm.value["email"])
       this.foodService.addUserProfileInformation(x.toString());
-      // //this.profileInformation[0] = this.registerForm.get("sex");
-      // this.profileInformation[0] = "female";
-      // this.profileInformation[1] =  150
-      // // this.profileInformation[2] = this.registerForm.get("height");
-      // this.profileInformation[2] = 66;
-      // this.profileInformation[3] = 0;
-      // this.profileInformation[4] = 0;
-      // this.profileInformation[5] = 0;
-
-      // this.foodService.editUserProfileInformation(x.toString(), this.profileInformation);
     })
-
-    
-
-    // this.profileInformation[0] = this.registerForm.get("sex");
-    // this.profileInformation[0] = "female";
-    // this.profileInformation[1] = this.registerForm.get("weight");
-    // // this.profileInformation[2] = this.registerForm.get("height");
-    // this.profileInformation[2] = 66;
-    // this.profileInformation[3] = 0;
-    // this.profileInformation[4] = 0;
-    // this.profileInformation[5] = 0;
-
-    // this.foodService.editUserProfileInformation(this.registerForm.value["email"], this.profileInformation);
   }
 
-//   async getUid() {
-//     // let user = await this.auth.auth().currentUser;
-//     // this.auth.authState.subscribe(data =>{
-//     //   of((this.foodService.editUserProfileInformation(data.email, this.informationArray))).subscribe(garbage => this.update())
-//     // })
-//     let email = await user.uid;
-// }
 }
